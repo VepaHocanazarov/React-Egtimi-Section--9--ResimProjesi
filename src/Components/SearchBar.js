5d5b70e5b25c14113650ac9440cee140c4d42a21
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-13 12:16:58 +0300

    SearchBar eklendi

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+
+       <input type="text" placeholder='Type anything for search ' />
+      
+    </div>
+  )
+}
+
+export default SearchBar
