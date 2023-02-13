46337f848b195014d957100d958ada5f7f0ae2fa
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-13 15:20:08 +0300

    serachinput entegrasyonu

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,24 +3,30 @@ import "./searchbar.css"
 
 class SearchBar extends Component {
 
-    state ={
-        search:""
+    state = {
+        search: ""
     }
 
-    inputChange = (event) =>
-    {
+    inputChange = (event) => {
 
         console.log(event.target.value)
 
-      this.setState({
-        search:event.target.value
-      })
+        this.setState({
+            search: event.target.value
+        })
+    };
+
+    serachinput = ()=>{
+        console.log("başarili")
     }
 
     render() {
         return (
             <div className=" serach-bar-container ui input">
-                <input type="text" onChange={this.inputChange} placeholder="Search..."  />
+                <input type="text" onChange={this.inputChange} placeholder="Search..." />
+                <button className="ui icon button" onClick={this.serachinput}>
+                    <i className="search icon"></i>
+                </button>
             </div>
         )
     }
