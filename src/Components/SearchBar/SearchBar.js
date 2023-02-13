a5838e42fd3a441b80ca712c6ff7ac21eeb8f865
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-13 15:06:07 +0300

    73. State e Değer Atama

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./searchbar.css"
 
-const SearchBar = () => {
-    return (
-        <div className=" serach-bar-container ui input">
-            <input type="text" placeholder="Search..." />
-        </div>
-    )
+class SearchBar extends Component {
+
+    state ={
+        search:""
+    }
+
+    inputChange(event){
+
+        console.log(event.target.value)
+
+      this.setState({
+        search:event.target.value
+      })
+    }
+
+    render() {
+        return (
+            <div className=" serach-bar-container ui input">
+                <input type="text" onChange={this.inputChange.bind(this)} placeholder="Search..."  />
+            </div>
+        )
+    }
 }
+
+
 
 export default SearchBar
