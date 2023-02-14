1fb7f162b70b8b0f42aace006a5afc789b29e92a
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-14 10:46:07 +0300

    App.js class componente çevrildi

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import "./App.css"
 
-const App = ()=>
-{
-  return(
-    <div className="app-container" >
-      <SearchBar/>
-    </div>
-  )
+class App extends Component {
+
+  onSearchİmage = (search)=>
+  {
+
+    console.log("App" + search)
+  }
+
+  render() {
+    return (
+      <div className="app-container" >
+        <SearchBar onSearchİmage={this.onSearchİmage} />
+      </div>
+    )
+  }
+
 }
 
 
